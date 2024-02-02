061b4104b44a71b8bcf0e9fb5124a274907cf103
bishal <bishalluitel6@gmail.com>
2024-02-02 20:56:08 +0545

    u

--- session.php
+++ session.php
@@ -1,7 +1,6 @@
 <?php
 session_start();
-echo "User ID: " . $_SESSION['user_id'] . "<br>";
-echo "Username: " . $_SESSION['username'] . "<br>";
+
 if (!isset( $_SESSION['username'])) {
 header('location: login.php');
 }
