578d3d4f30e12f043e36c44df44e96edd46bf3ae
bishal <bishalluitel6@gmail.com>
2024-02-02 20:37:19 +0545

    r

--- session.php
+++ session.php
@@ -3,3 +3,7 @@ if (isset($_SESSION['id'], $_SESSION['username'])) {
 header('location:login.php');
 }
 ?>
+<html>
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css">
+    <script src="https://cdn.tailwindcss.com"></script>
+</html>
