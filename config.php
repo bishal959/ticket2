6a65836638dee38eb016b2a543c425257c3b07b8
bishal <bishalluitel6@gmail.com>
2024-02-09 15:20:23 +0545

    s

--- config.php
+++ config.php
@@ -10,9 +10,12 @@ $username = "epiz_34107324";
 $password = "bb7P9jJ0VpqzCv";
 $dbname = "epiz_34107324_movie";
 
+
 $conn = new mysqli($servername, $username, $password, $dbname);
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
+}else{
+     echo "Connected successfully";
 }
 ?>
