e9c77ed88beed56dfc103a70b48bc72f0f27363f
bishal <bishalluitel6@gmail.com>
2024-02-25 14:15:16 +0545

    ]

--- config.php
+++ config.php
@@ -1,16 +1,10 @@
 <?php
-$envFile = __DIR__ . '/.env';
-$envVariables = parse_ini_file($envFile);
-$smtp_pass=$envVariables['SMTP_PASS'];
-$servername = $envVariables['servername'];
-$username = $envVariables['username'];
-$password = $envVariables['password'];
-$dbname = $envVariables['dbname'];
 
-// $servername = "localhost";
-// $username = "root";
-// $password = "";
-// $dbname = "movie";
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "movie";
 
 
 $conn = new mysqli($servername, $username, $password, $dbname);
